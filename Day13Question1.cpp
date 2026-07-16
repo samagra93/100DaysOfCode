3e03f4da60693158f7eb0e5bc0f0baa9a3d63f65
Samagra Gupta <gsamagra93@gmail.com>
2026-07-16 23:56:30 +0530

    Day13

---
+++ Day13Question1.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+    public:
+    void MatrixWalk(vector<vector<int>>& matrix) {
+      int m = matrix.size();
+      int n = matrix[0].size();
+
+      int top,bottom,left,right;
+      top=0;    
+      bottom=m-1;
+      left=0;
+      right=n-1;
+
+       while (top <= bottom && left <= right) {
+        // Top row
+        for (int j = left; j <= right; j++) {
+            printf("%d ", mat[top][j]);
+        }
+        top++;
+        
+        // Right column
+        for (int i = top; i <= bottom; i++) {
+            printf("%d ", mat[i][right]);
+        }
+        right--;
+        
+        // Bottom row (if exists)
+        if (top <= bottom) {
+            for (int j = right; j >= left; j--) {
+                printf("%d ", mat[bottom][j]);
+            }
+            bottom--;
+        }
+        
+        // Left column (if exists)
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--) {
+                printf("%d ", mat[i][left]);
+            }
+            left++;
+        }
+    }
+    
+    cout << endl;
+    }
+
+int main()
+{
+    int m, n;
+    cin >> m >> n;
+
+    vector<vector<int>> matrix(m, vector<int>(n));
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            cin >> matrix[i][j];
+
+    Solution obj;
+    obj.MatrixWalk(matrix);
+    
+}
